a4c3a7b49f0ed29970aa24d14c4dfe11be9f686d
whenwhathuh <johnnypr@udel.edu>
2019-11-09 17:13:33 -0500

    add gpio registers struct

--- drivers/inc/stm32f407xx.h
+++ drivers/inc/stm32f407xx.h
@@ -8,6 +8,8 @@
 #ifndef INC_STM32F407XX_H_
 #define INC_STM32F407XX_H_
 
+#include <stdint.h>
+
 /*
  *  base address of Flash and SRAM memory
  */
@@ -69,6 +71,21 @@
 #define SPI1_BASEADDR				(APB2PERIPH_BASEADDR + 0X3000)
 #define SYSCFG_BASEADDR				(APB2PERIPH_BASEADDR + 0X3800)
 #define EXTI_BASEADDR				(APB2PERIPH_BASEADDR + 0X3C00)
+
+/*************Peripheral Register Definition Structures***********/
+
+typedef struct {
+	volatile uint32_t MODER;
+	volatile uint32_t OTYPER;
+	volatile uint32_t OSPEEDR;
+	volatile uint32_t PUPDR;
+	volatile uint32_t IDR;
+	volatile uint32_t ODR;
+	volatile uint32_t BSRR;
+	volatile uint32_t LCKR;
+	volatile uint32_t AFRL;
+	volatile uint32_t AFRH;
+}GPIO_RegDef_t;
 
 #endif /* INC_STM32F407XX_H_ */
 
