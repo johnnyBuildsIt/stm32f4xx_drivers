49f91fd6b71d9eba4deefa2d54a27662523d3ba5
whenwhathuh <johnnypr@udel.edu>
2019-11-12 12:58:38 -0500

    move structures to header file

--- drivers/inc/stm32f407xx_gpio_driver.h
+++ drivers/inc/stm32f407xx_gpio_driver.h
@@ -10,5 +10,27 @@
 
 #include "stm32f407xx.h"
 
+/*
+ * config structure for a GPIO pin
+ */
+typedef struct {
+	uint8_t GPIO_PinNumber;
+	uint8_t GPIO_PinMode;
+	uint8_t GPIO_PinSpeed;
+	uint8_t GPIO_PinPuPdControl;
+	uint8_t GPIO_PinOPType;
+	uint8_t GPIO_PinAltFunMode;
+}GPIO_PinConfig_t;
+
+
+/*
+ * Handle structure for a GPIO pin
+ */
+
+typedef struct {
+	GPIO_RegDef_t *pGPIOBx; // pointer to hold the base address of the GPIO peripheral
+	GPIO_PinConfig_t GPIO_PinConfig; // GPIO pin configuration settings
+}GPIO_Handle_t;
+
 
 #endif /* INC_STM32F407XX_GPIO_DRIVER_H_ */
